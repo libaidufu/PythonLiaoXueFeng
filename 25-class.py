81967b3c4ba9ef1cf986abdb2f5c2c7e126b4fe6
libaidufu <ky1289044806@outlook.com>
2019-11-22 16:27:03 +0800

    understand1

--- 25-class.py
+++ 25-class.py
@@ -1,6 +1,8 @@
-
+# 类名一般是大写开头的字母
 class Student(object):
 	"""docstring for Student"""
+	# self本身也是传入函数内部的参数，即实例(instance)本身
+	# 一般来说，函数，包括类内部的方法，只可以使用函数内部的变量和传入函数内部的参数，一般不使用全局变量
 	def __init__(self, name, score):
 		self.name = name
 		self.score = score
@@ -18,6 +20,8 @@ print(bart.score)
 
 
 '''
+私有数据成员
+
 如果要让内部属性不被外部访问，可以把属性的名称前加上两个下划线__
 
 在Python中，实例的变量名如果以__开头，就变成了一个私有变量（private），只有内部可以访问，外部不能访问
@@ -26,7 +30,9 @@ print(bart.score)
 '''
 class Student2(object):
 	"""docstring for Student"""
+	# self是类内部方法默认的函数参数
 	def __init__(self, name, score):
+		self._Student2__name = None
 		self.__name = name
 		self.__score = score
 	def print_score(self):
@@ -36,9 +42,13 @@ class Student2(object):
 
 bartt = Student2('bart', 59)
 bartt.print_score()
-bartt.swecore=89
-print(bartt.swecore)
-print(bartt.get_name())
+# 创建了一个新的数据成员
+bartt.swecore = 89
+bartt.__init__('libai',90)
+bartt._Student2__name = 'nihk'
+bartt.print_score()
+# print(bartt.swecore)
+# print(bartt.get_name())
 '''
 双下划线开头的实例变量是不是一定不能从外部访问呢？
 
@@ -47,4 +57,5 @@ print(bartt.get_name())
 
 所以，仍然可以通过_Student__name来访问__name变量：
 '''
-print(bartt._Student2__name)
+
+# print(bartt._Student2__name)
